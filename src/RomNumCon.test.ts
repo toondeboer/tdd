4d36f485faf719ab47f6e4a4bfd8c46a1b3702d5
Toon de Boer <toondeboer@Toons-MacBook-Pro.local>
2022-09-27 15:39:24 +0200

    implemented numbers up until 49

--- src/RomNumCon.test.ts
+++ src/RomNumCon.test.ts
@@ -15,6 +15,20 @@ describe('test first rom num com', () =>{
         ${8} | ${'VIII'}
         ${9} | ${'IX'}
         ${10} | ${'X'}
+        ${11} | ${'XI'}
+        ${12} | ${'XII'}
+        ${13} | ${'XIII'}
+        ${14} | ${'XIV'}
+        ${15} | ${'XV'}
+        ${16} | ${'XVI'}
+        ${17} | ${'XVII'}
+        ${18} | ${'XVIII'}
+        ${19} | ${'XIX'}
+        ${20} | ${'XX'}
+        ${39} | ${'XXXIX'}
+        ${40} | ${'XL'}
+        ${41} | ${'XLI'}
+        ${49} | ${'XLIX'}
         `(`should parse $input to $output`, ({input, output}) => {
         const RomNum = NumConv(input);
         expect(RomNum).toStrictEqual(output);
--- src/RomNumCon.ts
+++ src/RomNumCon.ts
@@ -1,6 +1,19 @@
 export function NumConv(num: number) {
 
-    const array = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
-    return array[num - 1];
+    const numbers = [1,4,5,9,10,40];
+    const symbols = ['I', 'IV', 'V', 'IX', 'X', 'XL'];
+
+    let index = symbols.length -1;
+    let result = '';
+
+    while (num > 0) {
+        while (num >= numbers[index]) {
+            result += symbols[index]
+            num -= numbers[index]
+        }
+        index--;
+    }
+
+    return result;
 }
 
